b7883eb6608ca83e4708a0dfe9a78542d88d3695
anirban0184 <geekay0184@gmail.com>
2024-12-24 20:11:16 +0600

    first comment

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+echo "ostad";
+echo "# Ostad-Assignments" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/anirban0184/Ostad-Assignments.git
+git push -u origin main
